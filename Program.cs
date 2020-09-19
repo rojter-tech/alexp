170abd015417e767d8c43d28d2d1a749c47f2f85
Daniel Reuter <daniel.reuter@atea.se>
2020-09-19 17:30:43 +0200

    first solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace alexp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool good = false;
+            int a; int b; int c; int d; // Deklarera dessa innan for loopen
+
+            for (int i = 0; i <= 500; i++)
+            {
+                // n % k beraknar resten nar n delas pa k
+                a = i % 2;
+                b = i % 3;
+                c = i % 5;
+                d = i % 7;
+                if (a == 1 & b == 1 & c == 1 & d == 1)
+                {
+                    good=true;
+                    Console.WriteLine(i.ToString() + " is good!");
+                }
+            }
+            // Nu har den korts 501 ganger
+
+            if (good) // Kollar om good = true
+            {
+                Console.WriteLine("Yay!");
+            }
+            else
+            {
+                Console.WriteLine("Buhu ...");
+            }
         }
     }
 }
+
