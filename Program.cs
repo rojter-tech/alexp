10a530bdf1fb5cd9aa79126cabb84f9652b9395a
Daniel Reuter <daniel.reuter@atea.se>
2020-09-19 16:55:11 +0200

    console template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
